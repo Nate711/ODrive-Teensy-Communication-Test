e04ef806552c35ba7ff8b6b70dca3cb4e622bd38
Nathan Kau <nathankau@gmail.com>
2018-07-01 13:18:14 -0700

    initial commit of odrive-teensy communication program

---
+++ src/main.cpp
@@ -0,0 +1,104 @@
+
+#include <ODriveArduino.h>
+#include "Arduino.h"
+
+// Printing with stream operator
+template<class T> inline Print& operator <<(Print &obj,     T arg) { obj.print(arg);    return obj; }
+template<>        inline Print& operator <<(Print &obj, float arg) { obj.print(arg, 4); return obj; }
+
+// ODrive object
+ODriveArduino odrive(Serial1);
+
+void setup() {
+  // ODrive uses 115200 baud
+  Serial1.begin(500000);
+
+  // Serial to PC
+  Serial.begin(115200);
+  while (!Serial) ; // wait for Arduino Serial Monitor to open
+
+  Serial.println("ODriveArduino");
+  Serial.println("Setting parameters...");
+
+  // In this example we set the same parameters to both motors.
+  // You can of course set them different if you want.
+  // See the documentation or play around in odrivetool to see the available parameters
+  for (int axis = 0; axis < 2; ++axis) {
+    Serial1 << "w axis" << axis << ".controller.config.vel_limit " << 22000.0f << '\n';
+    Serial1 << "w axis" << axis << ".motor.config.current_lim " << 11.0f << '\n';
+    // This ends up writing something like "w axis0.motor.config.current_lim 10.0\n"
+  }
+
+  Serial.println("Ready!");
+  Serial.println("Send the character '0' or '1' to calibrate respective motor (you must do this before you can command movement)");
+  Serial.println("Send the character 's' to exectue test move");
+  Serial.println("Send the character 'b' to read bus voltage");
+  Serial.println("Send the character 'p' to read motor positions in a 10s loop");
+  Serial.println("Send the character 't' to send a test motor current and read back the motor positions");
+}
+
+void loop() {
+
+  if (Serial.available()) {
+    char c = Serial.read();
+
+    // Run calibration sequence
+    if (c == '0' || c == '1') {
+      int requested_state;
+
+      requested_state = ODriveArduino::AXIS_STATE_MOTOR_CALIBRATION;
+      Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
+      odrive.run_state(atoi(&c), requested_state, true);
+
+      requested_state = ODriveArduino::AXIS_STATE_ENCODER_OFFSET_CALIBRATION;
+      Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
+      odrive.run_state(atoi(&c), requested_state, true);
+
+      requested_state = ODriveArduino::AXIS_STATE_CLOSED_LOOP_CONTROL;
+      Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
+      odrive.run_state(atoi(&c), requested_state, false); // don't wait
+    }
+
+    // Sinusoidal test move
+    if (c == 's') {
+      Serial.println("Executing test move");
+      for (float ph = 0.0f; ph < 6.28318530718f; ph += 0.01f) {
+        float pos_m0 = 20000.0f * cos(ph);
+        float pos_m1 = 20000.0f * sin(ph);
+        odrive.SetPosition(0, pos_m0);
+        odrive.SetPosition(1, pos_m1);
+        delay(5);
+      }
+    }
+
+    // Read bus voltage
+    if (c == 'b') {
+      Serial1 << "r vbus_voltage\n";
+      Serial << "Vbus voltage: " << odrive.readFloat() << '\n';
+    }
+
+    // print motor positions in a 10s loop
+    if (c == 'p') {
+      static const unsigned long duration = 10000;
+      unsigned long start = millis();
+      while(millis() - start < duration) {
+        for (int motor = 0; motor < 2; ++motor) {
+          Serial1 << "r axis" << motor << ".encoder.pos_estimate\n";
+          Serial << odrive.readFloat() << '\t';
+        }
+        Serial << '\n';
+      }
+    }
+
+    /****** NATHAN CUSTOM CODE ******/
+    if(c == 't') {
+      long tic = micros();
+      odrive.SetDualCurrent(0.0, 0.0);
+      String in = odrive.readString();
+      long toc = micros();
+      Serial << "Motor positions: " << in << "\n";
+
+      Serial << "Out-in latency: " << toc-tic << "\n";
+    }
+  }
+}
